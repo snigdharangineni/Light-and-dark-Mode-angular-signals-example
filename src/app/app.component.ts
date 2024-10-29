e743e84eb55baca50232845b7b1d74d98fbbe848
snigdha <sunny.rangineni@gmail.com>
2024-10-29 12:44:29 -0500

    Signals Use case

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { signal, effect, computed } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -23,6 +24,12 @@ export class AppComponent {
 
   totalPrice = computed(() => this.price * this.quantity())
 
+  products = signal([
+    {id: 1, name: "Milk", price: 1.45},
+    {id: 2, name: "Bread", price: 3.90},
+    {id: 3, name: "Tomatoes", price: 2.20}
+  ])
+
   constructor() {
     effect(() => {
       this.label = this.theme();
@@ -32,9 +39,23 @@ export class AppComponent {
   toggleDarkMode() {
     this.theme.update(currentValue => currentValue === 'light'? 'dark' : 'light');
   }
-  
+
   changeQuantity(event: Event) {
     this.quantity.set((event.target as HTMLInputElement).valueAsNumber);
+  }
+
+  filterName = signal('');
+
+  filteredProducts = computed(() => {
+    return this.products().filter( 
+      product => product.name
+      .toLowerCase()
+      .includes(this.filterName().toLowerCase()))
+  });
+
+  changeFilter(event: Event) {
+    let newFilterName = (event.target as HTMLInputElement).value;
+    this.filterName.set(newFilterName);
   }
 
 }
